456b01d041f9896595b3a20458b9815e21386a89
Olena Kaliuzhna <kaliuzhna.el@gmail.com>
2023-03-28 23:26:35 +0300

    attach a listener to a parent element

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const smilesAll = document.querySelectorAll(".smile");
 const counters = document.querySelectorAll(".counter");
+const voting = document.querySelectorAll(".voting");
 
 console.log(smilesAll);
 console.log(counters);
@@ -13,11 +14,18 @@ function increaseValue(counter) {
     }
 }
 
-smilesAll.forEach((currentSmile, index) => {
-    currentSmile.addEventListener("click", increaseValue(counters[index]));
+// smilesAll.forEach((currentSmile, index) => {
+//     currentSmile.addEventListener("click", increaseValue(counters[index]));
+// })
+
+// You need to attach a listener to a parent element, then check event.target
+
+voting.addEventListener("click", (event) => {
+  if (event.target.contain("smile")) {
+    increaseValue();
+  }
 })
-
-
+// According to your markup parent element, you need to use the tr
 
 
 
